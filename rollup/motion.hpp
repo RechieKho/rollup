39358d6740fa9175c4e1b6589cb621be85edfe81
RechieKho <the.real.richie.kho@gmail.com>
2024-04-23 22:24:06 +0800

    Change `set_rotation` to `set_angular_velocity`.

--- rollup/motion.hpp
+++ rollup/motion.hpp
@@ -14,10 +14,16 @@ public:
     virtual ~Motion() = 0;
 
     /// Set the velocity.
+    /// When `p_velocity > 0`, move forward.
+    /// When `p_velocity == 0`, stop moving.
+    /// When `p_velocity < 0`, move backward.
     virtual void set_velocity(Int8 p_veloctiy) = 0;
 
-    /// Set the rotation.
-    virtual void set_rotation(Int16 p_degree) = 0;
+    /// Set the angular velocity.
+    /// When `p_degree > 0`, rotate to right.
+    /// When `p_degree == 0`, rotate to left.
+    /// When `p_degree < 0`, rotate to left.
+    virtual void set_angular_velocity(Int8 p_degree) = 0;
 };
 
 #endif // MOTION_HPP
